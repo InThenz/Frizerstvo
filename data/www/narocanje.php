6e45ebcb9f31e6d2ed9c34cc7c1a4c855fd77196
Jan <jan.kordez1@student.um.si>
2026-05-14 19:02:41 +0200

    Pregledal kaj mamo za 7. nalogo
    Pripravljeno za začetek PHP pisanja.

--- data/www/narocanje.php
+++ data/www/narocanje.php
@@ -1,5 +1,6 @@
 <?php
 include 'db.php';
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +12,7 @@ include 'db.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-    <title>Frizerstvo Friderik</title>
+    <title>Naročanje - Friderik</title>
 </head>
 <body>
     <?php include 'header.php'; ?>
@@ -53,4 +54,6 @@ include 'db.php';
 <script src="koledar.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
+
 </html>
--- data/www/ponudba.php
+++ data/www/ponudba.php
@@ -11,7 +11,7 @@ include 'db.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
-    <title>Frizerstvo Friderik</title>
+    <title>Ponudba - Friderik</title>
 </head>
 <body>
     <?php include 'header.php'; ?>
